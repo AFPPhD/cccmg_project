8f8ca0723e6c84135e175d17e21b2eabb64fbc18
Anthony Prime <boeing797@hotmail.co.uk>
2020-10-15 18:47:42 +0100

    Tidy up
    Changed ids to classes.

--- js/hide_no_javascript_mes.js
+++ js/hide_no_javascript_mes.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   
-    $('#nojavascript').remove();   // remove action makes element disappear
+    $('.nojavascript').remove();   // remove action makes element disappear
 	
-	$('#clickbandtext').show();    // show click band for info text
+	$('.clickbandtext').show();    // show click band for info text
 	
-	$('#navigation h3.hidden, #navigation ul.hidden').removeClass('hidden'); // Hide Navigation bar when javascript disabled 270511
+	$('.navigation h3.hidden, .navigation ul.hidden').removeClass('hidden'); // Hide Navigation bar when javascript disabled 270511
 
 });
