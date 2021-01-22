8393a7afd0ad698501ce067f9115aa99387e5f38
Anthony Prime <boeing797@hotmail.co.uk>
2021-01-22 17:54:52 +0000

    ID to Class
    Reduce element selection problems by using the less specific .navigation as opposed to #navigation

--- js/floating_nav_menu.js
+++ js/floating_nav_menu.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $(window).scroll(function() {
-  $('#navigation')
+  $('.navigation')
       .stop()                                                     // stop() - To prevent jittery effect due to animations queuing up
       .animate({top: $(document).scrollTop()},400,'easeOutQuad'); // Note: The window i.e document, is a javascript property so enclose with jquery function to select it $()
-  }); 
+  });
 });
