83f494a55ff0839f524df814294473840af2c817
Anthony Prime <boeing797@hotmail.co.uk>
2021-09-24 17:25:03 +0100

    Update hide_no_javascript_mes.js
    Delete remove nav menu  hidden class, since menu needs to be visible even if javascript disabled

--- js/hide_no_javascript_mes.js
+++ js/hide_no_javascript_mes.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {
-  
-    $('.nojavascript').remove();   // remove action makes element disappear
-	
-	$('.clickbandtext').show();    // show click band for info text
-	
-	$('.navigation h3.hidden, .navigation ul.hidden').removeClass('hidden'); // Hide Navigation bar when javascript disabled 270511
+$(document).ready(function() { // Use jQuery(function() {});
 
+    $('.nojavascript').remove();   // remove action makes element disappear
+
+	$('.clickbandtext').show();    // show click band for info text
+
+	// $('.navigation h3.hidden, .navigation ul.hidden').removeClass('hidden'); //Don't actually want to hide navigation menu
+	// when javascript disabled. Want to allow once click functionality. Screen readers ???
 });
